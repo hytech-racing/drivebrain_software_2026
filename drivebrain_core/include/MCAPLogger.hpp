271ee8168eeef89695dd57b87a2d76c49eb7577d
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2025-11-10 16:16:02 -0500

    saving

---
+++ drivebrain_core/include/MCAPLogger.hpp
@@ -0,0 +1,25 @@
+#include <mcap/writer.hpp>
+#include <deque> 
+
+
+namespace core {
+
+    class MCAPLogger {
+
+        public: 
+
+            /**
+             * 
+             */
+            MCAPLogger() {
+
+            }
+
+            
+
+        private: 
+
+
+    };
+
+}
