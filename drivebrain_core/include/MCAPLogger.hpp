39447b6e5fa4df5209d261e1e8bae1b297321fa9
Warren Yun <wyun2006@gmail.com>
2026-01-22 20:19:40 -0500

    Live parameter handling (#10)
    * param log update fixes
    
    * Add param change boost signal
    
    * Param logging methods (not done)
    
    * get rid of configurable
    
    * Finished parameter schema logging
    
    * fix parameter type conversion conflicts
    
    * remove mcap used for testing
    
    * Add multi level JSON schema
    
    * add param mutex to get_all_params
    
    * Update drivebrain_core/src/FoxgloveServer.cpp
    
    Co-authored-by: Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
    
    * cleanup
    
    * More cleanup
    
    ---------
    
    Co-authored-by: Krish Kittur <kittur.krish@gmail.com>
    Co-authored-by: Krish Kittur <73321517+KrishKittur@users.noreply.github.com>

--- drivebrain_core/include/MCAPLogger.hpp
+++ drivebrain_core/include/MCAPLogger.hpp
@@ -1,3 +1,5 @@
+#pragma once 
+
 #include <deque> 
 #include <thread> 
 #include <mutex> 
@@ -9,6 +11,7 @@
 #include <mcap/writer.hpp>
 #include <google/protobuf/descriptor.pb.h>
 #include <foxglove/websocket/base64.hpp>
+#include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 
 namespace core {
@@ -30,7 +33,7 @@ namespace core {
              * @param base_dir the directory in which the log file should be created
              * @param options options to create the mcap with 
              */
-            static void create(const std::string &base_dir, const mcap::McapWriterOptions &options);
+            static void create(const std::string &base_dir, const mcap::McapWriterOptions &options, const std::string &params_file);
 
             /**
              * Fetches MCAPLogger singleton instance
@@ -106,28 +109,23 @@ namespace core {
             int log_msg(MsgType message); 
 
             /**
-             * Logs the json params to the current MCAP file. Doesn't need an input because the schema has already been created. 
-             * 
+             * Logs the json params to the current MCAP file
+             *
+             * @param The parameters to be logged, in JSON format
              * @return 0 on success, negative err code on failure
              */
-            int log_params();
+            int log_params(nlohmann::json params);
 
         private: 
           
             /* Private constructor to be called by init method */
-            MCAPLogger(const std::string &base_dir, const mcap::McapWriterOptions &options);
+            MCAPLogger(const std::string &base_dir, const mcap::McapWriterOptions &options, const std::string &params_file);
 
             /**
              * Spawned by thread, loops until end of program life or error occurs. 
              * Logs all queued messages to a file. 
              */
             void _handle_log_to_file();     
-
-            /**
-             * Spawned by thread, loops until end of program life or error occurs.
-             * Logs all active params.
-            */
-            void _handle_param_log();
 
             /* Singleton move semantics */
             MCAPLogger(const MCAPLogger&) = delete;
@@ -152,9 +150,10 @@ namespace core {
             mcap::McapWriterOptions _options;
             
             /* State */
+            nlohmann::json _params_schema_json;
+            nlohmann::json _initial_params;
             std::unordered_map<std::string, uint32_t> _name_to_id_map;
             std::string _log_name = "NONE";
-            bool _param_schema_written = false;
             bool _logging = false;
             bool _running = true;
 
