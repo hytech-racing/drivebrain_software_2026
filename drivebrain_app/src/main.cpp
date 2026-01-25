80ac976468a8192e3d854edfa7d1ff6d519aa832
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2026-01-25 12:36:21 -0500

    Feature/better cmdline parsing (#12)
    * add clean argument parsing
    
    * more specific error message

--- drivebrain_app/src/main.cpp
+++ drivebrain_app/src/main.cpp
@@ -11,8 +11,55 @@
 #include <mcap/writer.hpp>
 #include <memory>
 #include <thread>
+#include <optional>
+#include <filesystem>
 
 std::atomic<bool> running = true;
+std::optional<std::string> json_file; 
+std::optional<std::string> dbc_file;
+
+int parse_arguments(int &argc, char* argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "c:d:")) != -1) {
+        switch (opt) {
+            case 'c': 
+                if (optarg != nullptr && strlen(optarg) > 0 && std::filesystem::exists(optarg)) {
+                    json_file = optarg;
+                } else {
+                    spdlog::error("Invalid json file provided: {}", optarg);
+                    return 1;
+                }
+                break;
+            case 'd': 
+                if (optarg != nullptr && strlen(optarg) > 0 && std::filesystem::exists(optarg)) {
+                    dbc_file = optarg;
+                } else {
+                    spdlog::error("Invalid dbc file provided: {}", optarg);
+                    return 1;
+                }
+                break;
+            case '?':
+                spdlog::error("Unknown command line option: -{}", static_cast<char>(optopt));
+                return 1;
+            default: 
+                spdlog::error("Could not parse command line arguments.");
+                return 1;
+        }
+    }
+
+    if (!json_file) {
+        spdlog::error("Did not receive the required json file argument.");
+        return 1;
+    }
+
+    if (!dbc_file) {
+        spdlog::error("Did not receive the required dbc file argument.");
+        return 1;
+    }
+
+    return 0;
+}
 
 void sig_handler(int signal) {
     if(signal == SIGINT) {
@@ -35,8 +82,16 @@ void get_param_task(int wait_time, core::MsgType msg) {
 
 int main(int argc, char* argv[]) {
 
-    core::FoxgloveServer::create(argv[1]);
-    core::MCAPLogger::create("recordings/", mcap::McapWriterOptions(""), argv[1]);
+    // Argument Handling
+    int return_code = parse_arguments(argc, argv);
+    if (return_code != 0) {
+        spdlog::error("Expected Usage: ./drivebrain -c path/to/config.json -d path/to/hytech.dbc");
+        return return_code;
+    }
+
+    // Singleton Creation
+    core::FoxgloveServer::create(json_file.value());
+    core::MCAPLogger::create("recordings/", mcap::McapWriterOptions(""), json_file.value());
     core::MCAPLogger::instance().open_new_mcap("test_1.mcap");
     core::MCAPLogger::instance().init_logging();
 
@@ -49,6 +104,7 @@ int main(int argc, char* argv[]) {
     auto acu_data = std::make_shared<hytech_msgs::ACUAllData>();
     acu_data->set_max_cell_temp_id(676767);
 
+    // Spawning main threads
     std::thread t1(get_param_task, 20, vel_msg);
     std::thread t2(get_param_task, 40, acu_data);
 
