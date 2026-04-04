41b048a311d17ff67e33bc8bb203ed7184d5d5cb
akemiyu <43460886+akemiyu@users.noreply.github.com>
2026-04-04 19:11:29 -0400

    Create VectorNav (#7)
    * Create VectorNav
    
    * Changed method bracket style
    
    * Renamed StateEstimator to StateTracker
    
    * include drivebrain comms in root build
    
    * Deleted Configurable & Loggable dependencies
    
    * Begin implementation of initial heading in Mode 0 to expedite startup to Mode 3 via declination offset of magnetic heading
    
    * minor fixes
    
    * update param fetching
    
    ---------
    
    Co-authored-by: Krish Kittur <kittur.krish@gmail.com>
    Co-authored-by: Kevin Luo <kevinluo423@gmail.com>
    Co-authored-by: nebudev14 <wyun2006@gmail.com>

--- tests/scripts/run_ethernet_driver.cpp
+++ tests/scripts/run_ethernet_driver.cpp
@@ -1,63 +1,63 @@
-/**
- * Script for testing ethernet driver functionality.
- * Spawns a mock thread sending protobuf messages over "ethernet" using the ETHSend class.
-*/
+// /**
+//  * Script for testing ethernet driver functionality.
+//  * Spawns a mock thread sending protobuf messages over "ethernet" using the ETHSend class.
+// */
 
-#include "ETHSendComms.hpp"
-#include "ETHRecvComms.hpp"
-#include <boost/asio/io_context.hpp>
-#include <chrono>
-#include <csignal>
-#include <google/protobuf/message.h>
-#include <iostream>
-#include <atomic> 
-#include <hytech_msgs.pb.h>
-#include <hytech.pb.h>
-#include <FoxgloveServer.hpp>
-#include <MCAPLogger.hpp>
-#include <mcap/writer.hpp>
-#include <memory>
-#include <thread>
+// #include "ETHSendComms.hpp"
+// #include "ETHRecvComms.hpp"
+// #include <boost/asio/io_context.hpp>
+// #include <chrono>
+// #include <csignal>
+// #include <google/protobuf/message.h>
+// #include <iostream>
+// #include <atomic> 
+// #include <hytech_msgs.pb.h>
+// #include <hytech.pb.h>
+// #include <FoxgloveServer.hpp>
+// #include <MCAPLogger.hpp>
+// #include <mcap/writer.hpp>
+// #include <memory>
+// #include <thread>
 
-std::atomic<bool> running = true;
-void sig_handler(int signal) {
-    if(signal == SIGINT) {
-        std::cout << "halting\n";
-        running = false;
-    }
-}
+// std::atomic<bool> running = true;
+// void sig_handler(int signal) {
+//     if(signal == SIGINT) {
+//         std::cout << "halting\n";
+//         running = false;
+//     }
+// }
 
-int main(int argc, char* argv[]) {
-    // Singleton Creation
-    core::FoxgloveServer::create(argv[1]);
-    core::MCAPLogger::create("recordings/", mcap::McapWriterOptions(""), argv[1]);
-    core::MCAPLogger::instance().open_new_mcap("test_1.mcap");
-    core::MCAPLogger::instance().init_logging();
+// int main(int argc, char* argv[]) {
+//     // Singleton Creation
+//     core::FoxgloveServer::create(argv[1]);
+//     core::MCAPLogger::create("recordings/", mcap::McapWriterOptions(""), argv[1]);
+//     core::MCAPLogger::instance().open_new_mcap("test_1.mcap");
+//     core::MCAPLogger::instance().init_logging();
 
-    std::signal(SIGINT, sig_handler);
+//     std::signal(SIGINT, sig_handler);
     
-    boost::asio::io_context io_context;
-    comms::ETHSendComms eth_sender{io_context, 2222, "127.0.0.1"};
-    comms::ETHRecvComms<hytech_msgs::ACUAllData> eth_recver{io_context, 2222};
+//     boost::asio::io_context io_context;
+//     comms::ETHSendComms eth_sender{io_context, 2222, "127.0.0.1"};
+//     comms::ETHRecvComms<hytech_msgs::ACUAllData> eth_recver{io_context, 2222};
 
-    std::thread io_thread([&]() {
-        io_context.run();
-    });
+//     std::thread io_thread([&]() {
+//         io_context.run();
+//     });
 
-    std::thread t1([&]() {
-        while(running) {
-            auto msg = std::make_shared<hytech_msgs::ACUAllData>();
-            msg->set_max_cell_temp_id(6767);
-            std::this_thread::sleep_for((std::chrono::seconds(1)));
-            eth_sender.enqueue_msg_send(msg);
-        }
-    });
+//     std::thread t1([&]() {
+//         while(running) {
+//             auto msg = std::make_shared<hytech_msgs::ACUAllData>();
+//             msg->set_max_cell_temp_id(6767);
+//             std::this_thread::sleep_for((std::chrono::seconds(1)));
+//             eth_sender.enqueue_msg_send(msg);
+//         }
+//     });
 
-    if(t1.joinable()) t1.join();
-    io_context.stop();
-    if(io_thread.joinable()) io_thread.join();
+//     if(t1.joinable()) t1.join();
+//     io_context.stop();
+//     if(io_thread.joinable()) io_thread.join();
     
-    core::MCAPLogger::instance().close_active_mcap();
-    core::MCAPLogger::destroy();
-    core::FoxgloveServer::destroy();
-}
+//     core::MCAPLogger::instance().close_active_mcap();
+//     core::MCAPLogger::destroy();
+//     core::FoxgloveServer::destroy();
+// }
